3e4e428a3aa0c87b26f8d395596d5a50b512d95a
Lilia Bekuzina <lilia.bekuzina1@gmail.com>
2023-04-07 21:05:40 +0200

    Initial commit

---
+++ src/test/java/se/enso/artshop/ArtShopApplicationTests.java
@@ -0,0 +1,13 @@
+package se.enso.artshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
